64ac1e0e9ad4e5f675c1ca506970123cdb3949aa
Kyle Swartz <kyle.swartz.3@gmail.com>
2017-02-01 20:33:09 -0500

    setup rspec and the db

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,31 @@ module Buddies
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+		config.autoload_paths << Rails.root.join('lib/services/**')
+
+		config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+
+    # Configure CORS rules for the API
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+
+        resource '/v1/*',
+          headers: :any,
+          methods: [:get, :post, :delete, :put, :options, :head],
+          credentials: true,
+          max_age: 600
+      end
+    end
+
+		# Cache store
+    config.cache_store = :redis_store, ENV.fetch("REDIS_URL", "redis://localhost:6379")
+
+    # Background jobs
+    config.active_job.queue_adapter = :sidekiq
+
   end
 end
